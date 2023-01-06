a25222e749193286174611f851f1ace8fc78ae70
xafable <xafable55@gmail.com>
2023-01-06 14:31:56 +0200

    new routes

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*
+ * Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::controller(\App\Http\Controllers\ApplicationController::class)
+    ->prefix('v1/application')
+    ->group(function () {
+
+    Route::get('/{id}', 'get');
+    Route::post('/create', 'create');
+    Route::get('/get/last', 'getFakes');
 
 
-Route::controller(\App\Http\Controllers\PriceController::class)->prefix('v1')->group(function () {
-    Route::get('/list', 'index');
+    });
+
+Route::controller(\App\Http\Controllers\PriceController::class)
+    ->prefix('v1/coins')
+    ->group(function () {
+
+    Route::get('/available ', 'available');
     Route::get('/get', 'get');
 
+});
+
+Route::controller(\App\Http\Controllers\TicketController::class)
+    ->prefix('v1/ticket')
+    ->group(function () {
+
+    Route::post('/create', 'create');
 });
