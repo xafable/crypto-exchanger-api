198ee66db4463c4babfd26b80e3e49ce505f7c26
xafable <xafable55@gmail.com>
2023-01-06 14:30:18 +0200

    service coinGecko fixes

--- app/Services/CoinGecko.php
+++ app/Services/CoinGecko.php
@@ -4,6 +4,7 @@
 namespace App\Services;
 
 
+use App\Models\CoinLov;
 use Codenixsv\CoinGeckoApi\CoinGeckoClient;
 
 class CoinGecko
@@ -15,19 +16,48 @@ class CoinGecko
         $this->client = new CoinGeckoClient();
     }
 
-    function ping(){
+    function ping() : array {
         return $this->client->ping();
     }
 
-    function getPriceList(){
+    function getPriceList() : array {
         return $this
             ->client
             ->simple()
-            ->getPrice('bitcoin,ethereum,tether,binancecoin,ripple', 'usd');
+            ->getPrice($this->getCoinsFromDb(), 'usd');
     }
 
-    function getPrice(){
-        return $this->client->simple()->getPrice('0x,bitcoin', 'usd,rub');
+    function getPrice(string $fromCoin,string $toCoin,float $fromCoinAmount) : float {
+        $geckoId = $this->getGeckoIdFromDb($fromCoin);
+        $fromCoinPrice = $this
+            ->client
+            ->simple()
+            ->getPrice($geckoId, 'usd')[$geckoId]['usd'];
+
+        $geckoId = $this->getGeckoIdFromDb($toCoin);
+        $toCoinPrice = $this
+            ->client
+            ->simple()
+            ->getPrice($geckoId, 'usd')[$geckoId]['usd'];
+
+        $toCoinAmount = $fromCoinPrice/$toCoinPrice*$fromCoinAmount;
+
+        return  $toCoinAmount;
+    }
+
+    private function getCoinsFromDb() : string {
+       return CoinLov::query()
+            ->get()
+            ->pluck('coin_gecko_id')
+            ->implode(',');
+    }
+
+    private function getGeckoIdFromDb(string $coinTitle) : string {
+        return CoinLov::query()
+            ->select('coin_gecko_id')
+            ->where('coin_title','=',$coinTitle)
+            ->value('coin_gecko_id');
+
     }
 
 }
